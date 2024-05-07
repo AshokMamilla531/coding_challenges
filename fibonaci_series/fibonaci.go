32bf88d9815035399469e40f4491b44677c67c99
AshokMamilla <ashok@getparsnip.com>
2024-05-08 00:34:34 +0100

    Added Recursion Fibonacci Logic

--- fibonaci_series/fibonaci.go
+++ fibonaci_series/fibonaci.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	max := 10
-	Fibonaci(max)
+	//Fibonaci(max)
+	for i := 0; i < max; i++ {
+		fmt.Println(RecFibnaci(i))
+	}
 }
 
 func Fibonaci(m int) {
@@ -17,4 +20,11 @@ func Fibonaci(m int) {
 		fmt.Println(next)
 		a, b = b, next
 	}
+}
+
+func RecFibnaci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return RecFibnaci(n-1) + RecFibnaci(n-2)
 }
