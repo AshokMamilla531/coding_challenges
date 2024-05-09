36709a4f12d55601e3b335858028c172df6432dc
AshokMamilla <ashok@getparsnip.com>
2024-05-09 03:37:24 +0100

    Palindrome

---
+++ string_palindrome/palindrome_check.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := "madxam"
+	isPal := IsPalindrome(input)
+	fmt.Println(isPal)
+}
+
+func IsPalindrome(input string) bool {
+
+	inputBytes := []byte(input)
+	i := 0
+	j := len(inputBytes) - 1
+	for i, j = 0, len(input)-1; i < len(input)-1 && j > 0; i, j = i+1, j-1 {
+		if inputBytes[i] != inputBytes[j] {
+			return false
+		}
+	}
+	return true
+
+}
